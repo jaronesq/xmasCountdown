136c96546755f07ea1792b1730f035b240d5c9dd
Jaron Esquivel <jaron.esq@gmail.com>
2018-11-28 17:17:51 -0700

    countdown start

---
+++ app.js
@@ -0,0 +1,8 @@
+//countdown to dec 25
+//date for today
+//date we want (dec 25)
+//calculation, to figure out how much time till dec 25
+//display on the html
+
+//eventlistener, onchange, the change of thee difference calculation
+
